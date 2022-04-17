bd4d768e7ea9e1107a53da9323cf710f95a0514e
Junjie Zhang <junjiezhang@Junjies-MacBook-Pro.local>
2022-04-17 12:01:28 -0400

    production

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header'
 import Home from './pages/Home'
 import Bottom from './component/Bottom'
+import React, { lazy } from 'react';
+
+// const Header = lazy(()=>import('./component/Header'));
+// const Home = lazy(()=>import('./pages/Home'));
+// const Bottom = lazy(()=>import('./component/Bottom'));
+
 
 function App() {
   return (
